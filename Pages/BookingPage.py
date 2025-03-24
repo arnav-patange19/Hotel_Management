315cc18da1945640a9b8805f1e403e91e2a5d90c
Arnav <arnxv987@gmail.com>
2025-03-24 20:58:05 +0530

    updated code

--- Pages/BookingPage.py
+++ Pages/BookingPage.py
@@ -1,21 +1,21 @@
 import streamlit as st
 import datetime
 def main():
-    st.title("Room Booking Page")
+    st.title("Booking Page")
 
     # Room Categories Section
     st.sidebar.header("Select Room Category")
-    room_category = st.sidebar.radio("Category", ["Cat 1", "Cat 2", "Cat 3"])
+    room_category = st.sidebar.radio("Category", ["Regular", "Deluxe", "King Bed"])
 
     # Booking Info Section
-    st.header("Booking Information")
+    st.header("Information")
     name = st.text_input("Name")
     age = st.number_input("Age", min_value=1, max_value=120, step=1)
     dob = st.date_input("Date of Birth", value=datetime.date(2000, 1, 1), min_value=datetime.date(1900, 1, 1))
     guests = st.number_input("Number of Guests", min_value=1, step=1)
 
     # Package Selection
-    package = st.selectbox("Select Package", ["Room Only", "Room + Resto Plan"])
+    package = st.selectbox("Select Package", ["Room Only", "Room + Resto"])
     duration = st.number_input("Duration of Stay (nights)", min_value=1, step=1)
 
     if st.button("Confirm Booking"):
